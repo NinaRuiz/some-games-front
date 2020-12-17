f5532c9056592e63cf38a71e156e9f9538a51194
NinaRuiz <nina.ruizandres@gmail.com>
2020-12-17 16:45:33 +0100

    Add toolbar to toolbar component

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LayoutsRoutingModule } from './layouts-routing.module';
 import { BasicComponent } from './layout/basic/basic.component';
 import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [BasicComponent, ToolbarComponent],
   imports: [
     CommonModule,
-    LayoutsRoutingModule
+    LayoutsRoutingModule,
+    MatToolbarModule
   ]
 })
 export class LayoutsModule { }
