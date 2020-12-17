50b3eeedfa6a8b225b4a924622bb7449566813d7
NinaRuiz <nina.ruizandres@gmail.com>
2020-12-17 16:44:29 +0100

    Add routing to basic layout

--- src/app/layouts/layouts-routing.module.ts
+++ src/app/layouts/layouts-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BasicComponent} from './layout/basic/basic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
