116455684b9a2c5b3e4ca9d78bf2c16d4dc7cd62
NinaRuiz <nina.ruizandres@gmail.com>
2020-12-17 16:39:01 +0100

    Created basic layout and toolbar component

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutsRoutingModule } from './layouts-routing.module';
+import { BasicComponent } from './layout/basic/basic.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BasicComponent, ToolbarComponent],
   imports: [
     CommonModule,
     LayoutsRoutingModule
