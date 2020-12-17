cb3c8ec40818f64fb2f623e02393314ef32a6cec
NinaRuiz <nina.ruizandres@gmail.com>
2020-12-17 16:47:39 +0100

    Changed toolbar app name and created Hangman game module

---
+++ src/app/modules/hangman-game/hangman-game.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HangmanGameRoutingModule } from './hangman-game-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HangmanGameRoutingModule
+  ]
+})
+export class HangmanGameModule { }
